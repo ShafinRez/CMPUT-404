bae372cd311c8dc30063a9b70b32d6b2e7116819
ShafinRez <shafinrezwan123@gmail.com>
2021-09-16 22:49:00 -0600

    lab 2 added

--- lab2/proxy_server.py
+++ lab2/proxy_server.py
@@ -5,33 +5,40 @@ HOST = ""
 PORT = 8001
 BUFFER_SIZE = 1024
 
-def get_remote_ip(host):
-    print(f'Getting IP for {host}')
+def get_remote_ip(extern_host):
+    print(f'Getting IP for {extern_host}')
     try:
-        remote_ip = socket.gethostbyname( host )
+        remote_ip = socket.gethostbyname(extern_host)
     except socket.gaierror:
         print ('Hostname could not be resolved. Exiting')
         sys.exit()
 
-    print (f'Ip address of {host} is {remote_ip}')
+    print (f'Ip address of {extern_host} is {remote_ip}')
     return remote_ip
 
 def handle_request():
-
+    
     return
 
 
 def main():
-    HOST = 'www.google.com'
+    HOST = ""
+    EXTERN_HOST = 'www.google.com'
+    PORT = 8001
+    BUFFER_SIZE = 1024
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_start:
         proxy_start.bind((HOST, PORT))
         proxy_start.listen()
-        conn, addr = proxy_start.accept()
-
+        
         while True:
-            
+            conn, addr = proxy_start.accept()
+            print("Connected by", addr)
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_end:
-                get_remote_ip(HOST)
+                ext = get_remote_ip(EXTERN_HOST)
+                proxy_end.bind((ext, PORT))
+
+                target = handle_request()
 
 
+                conn.close()
 main()
